860738684a5e2ac607a03a5659b0c2fa5b26baf4
Cade Brown <brown.cade@gmail.com>
2016-10-22 18:30:32 -0400

    Updated compiler

--- EZcompiler.py
+++ EZcompiler.py
@@ -4,6 +4,7 @@ from shared_data import *
 import shared_data
 
 default_libs = ["libBasic", "libLoops", "libMath"]
+
 char_st = {
 
 }
@@ -11,6 +12,10 @@ char_st = {
 char_op = {
 
 }
+
+multiline_comment = False
+
+line_ct = 1
 
 
 def import_libs_code(imports):
@@ -22,6 +27,7 @@ def import_libs_code(imports):
 def build_classes(imports):
     global char_st
     global char_op
+
     char_st = dict()
     char_op = dict()
     for l in imports:
@@ -39,8 +45,13 @@ def build_classes(imports):
         char_st = dict(char_st.items() + st_imports.items())
         char_op = dict(char_op.items() + op_imports.items())
 
+def transpile_sts(list_st):
+    r = ""
+    for st in list_st:
+        r += "\t\n" + st.get_st()
+    return r
 
-def compile_str(lines, imports):
+def compile_lines(lines, imports):
     imports = imports + default_libs
     exec(import_libs_code(imports))
     
@@ -51,12 +62,13 @@ def compile_str(lines, imports):
     statement_str = ""
 
     for line in lines:
-        try:
-            v = pile_str(line)
-            if v:
+        v = pile_str(line)
+        if v:
+            if isinstance(v, list):
+                for s in v:
+                    statements.append(s)
+            else:
                 statements.append(v)
-        except Exception as e:
-            log.err("GenericLine", str(e))
 
     CONSTS_ST = [
         eval("%s(assign=\"prec\", args=[\"128\"])" % (char_st["setprec"])),
@@ -64,55 +76,83 @@ def compile_str(lines, imports):
         eval("%s(assign=\"ONE\", args=[\"1.0\"])" % (char_st["set"])),
         eval("%s(assign=\"TWO\", args=[\"2.0\"])" % (char_st["set"])),
         eval("%s(assign=\"TEN\", args=[\"10.0\"])" % (char_st["set"]))
-        ]
+    ]
 
     for st in statements:
         if isinstance(st, eval(char_st["setprec"])):
-            print "Found"
             CONSTS_ST[0] = st
             statements.remove(st)
 
     statements = CONSTS_ST[1:] + statements
 
-    out += CONSTS_ST[0].get_st() + "\n"
+    out += "\n\t" + CONSTS_ST[0].get_st()
 
-    for st in statements:
-        statement_str += st.get_st() + "\n"
+    statement_str += transpile_sts(statements)
 
     var_names.remove("prec")
 
     for v in var_names:
-        out += get_var_init(v) + "\n"
+        out += "\n\t" + get_var_init(v)
 
     out = out + statement_str
 
     for v in var_names:
-        out += get_var_free(v) + "\n"
+        out += "\n\t" + get_var_free(v)
 
     out += end
     return out
 
-def pile_str(line):
-    if "#" in line:
-        line = line[:line.index("#")]
-    line = clean_line(line)
-    if line == "":
-        return
-    for char in char_op:
-        if " " + char + " " in line and "=" in line:
-            _data = clean_line(line.replace(char, ""))
+def parse_oper(line):
+    for item in char_op:
+        if " " + item + " " in line and "=" in line:
+            _data = clean_line(line.replace(item, ""))
             assign, args = parse_function(_data)
             a, b = args
-            exec("R=%s(assign, a, b)" % (char_op[char]))
+            exec("R = %s(assign, a, b)" % (char_op[item]))
             return R
-    for char in char_st:
-        if (char == line) or (char + " " in line):
-            if (line.index(char) == 0) or (" %s " % (char) in line):
-                rm_func = line.replace(char, "", 1) 
-                assign, args = parse_function(rm_func)
-                exec("R=%s(assign=assign, args=args)" % (char_st[char]))
-                return R
+
+def parse_st(line):
+    for item in char_st:
+        if (line == item) or (line.startswith(item) and ("%s " % (item)) in line) or (" %s " % (item) in line):
+            rm_func = line.replace(item, "", 1)
+            assign, args = parse_function(rm_func)
+            exec("R = %s(assign=assign, args=args)" % (char_st[item]))
+            return R
+
+def parse_default(line):
     if "=" in line:
         assign, args = parse_function(line)
-        exec("R=%s(assign=assign, args=args)" % (char_st["set"]))
+        exec("R = %s(assign=assign, args=args)" % (char_st["set"]))
         return R
+
+def pile_str(line, level=0):
+    global line_ct
+    global multiline_comment
+    R = None
+    if line.startswith("###"):
+        multiline_comment = not multiline_comment
+    
+    read_line = not (line.startswith("#") or multiline_comment)
+
+    if read_line:
+        try:
+            if "#" in line:
+                line = line[:line.index("#")]
+            line = clean_line(line)
+
+            if ";" in line:
+                Rs = []
+                for sl in line.split(";"):
+                    Rs.append(pile_str(clean_line(sl), level + 1))
+                return Rs
+
+            R = parse_oper(line)
+            if not R:
+                R = parse_st(line)
+            if not R:
+                R = parse_default(line)
+        except Exception as e:
+            log.err("Transpiling", ["Line %s: " % (line_ct), "  %s" % (line)])
+    if level == 0:
+        line_ct += 1
+    return R
--- EZlogger.py
+++ EZlogger.py
@@ -2,15 +2,31 @@ name = "EZCC"
 
 version = "0.1.0"
 
-info = """%s v%s
-Chemical Development 2016
-
-chemicaldevelopment.us
-""" % (name, version)
-
+HEADER = '\033[95m'
+OKBLUE = '\033[94m'
+OKGREEN = '\033[92m'
+WARNING = '\033[93m'
+FAIL = '\033[91m'
+ENDC = '\033[0m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
 
 def init():
-    print info
+    base_print(OKBLUE, "EZCC", "Initializing")
 
-def err(task, err):
-    print "%s: \n\t%s" % (task, err)
+def base_print(col, task, st):
+    print "%s v%s: \n  %s:" % (col+name, WARNING+version+col, task)
+    
+    if isinstance(st, list):
+        for s in st:
+            print "    %s" % (OKGREEN + s)
+    else:
+        print "    %s" % (OKGREEN + st)
+    print "%s" % (HEADER) 
+
+def info(task, st):
+    base_print(OKBLUE, task, st)
+
+def err(task, error):
+    base_print(FAIL, task, error)
+
