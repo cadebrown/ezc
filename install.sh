a5293cf05c2c8f57138677d23c45a92c1154e51c
Cade Brown <brown.cade@gmail.com>
2016-10-25 10:44:28 -0400

    test

--- install.sh
+++ install.sh
@@ -1,28 +1,3 @@
 #!/bin/bash
-
-INSTALL_DIR=$1
-LINK=$2
-SOURCES="ezcc EZcompiler.py EZlogger.py libMath.py libLoops.py libBasic.py shared_data.py"
-
-if [ "$1" == "" ] || [ "$1" == "auto" ]; then
-    INSTALL_DIR=/usr/local/ezc
-fi
-
-if [[ "$2" == "" ]]; then
-    LINK=/usr/bin/ezcc
-fi
-
-echo Installing EZC in $INSTALL_DIR
-
-mkdir -p $INSTALL_DIR
-cp -t $INSTALL_DIR $SOURCES
-
-echo Done copying
-
-if [[ "$LINK" != "none" ]]; then
-    rm $LINK
-    echo Now linking to $LINK
-    ln -s $INSTALL_DIR/ezcc /usr/bin/ezcc
-fi
-
-echo "If you got any permissions errors, please run with sudo"
+brew install mpfr
+apt-get install libmpfr-dev libmpfr-doc libmpfr4 libmpfr4-dbg
