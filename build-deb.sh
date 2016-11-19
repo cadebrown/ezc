37fea20454d9430457d18900afdb915b870ef206
Cade Brown <brown.cade@gmail.com>
2016-11-18 22:45:44 -0500

    Updated build process

--- build-deb.sh
+++ build-deb.sh
@@ -11,9 +11,9 @@ if [ "$VERSION" == "" ]; then
 	echo "Default version"
 	VERSION="latest"
 fi
-
+mkdir -p deb-package/usr/bin/
 cp dist/ezcc deb-package/usr/bin/ezcc
-gzip -9 -c changelog > deb-package/usr/share/doc/ezcc/changelog.gz
+gzip -n -9 -c changelog > deb-package/usr/share/doc/ezcc/changelog.gz
 
 echo "Copied in, now making deb file"
 
@@ -26,6 +26,7 @@ chmod 0755 deb-package/usr/share/
 chmod 0755 deb-package/usr/share/doc/
 chown root:root deb-package/usr/share/doc/ezcc
 chmod 0755 deb-package/usr/share/doc/ezcc
+chmod 0644 deb-package/usr/share/doc/ezcc/copyright
 
 fakeroot dpkg-deb --build deb-package $FL && lintian $FL
 
