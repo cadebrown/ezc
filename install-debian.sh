9b895813e8a7b3c445175bd72e453ae630605261
Cade Brown <brown.cade@gmail.com>
2016-11-23 12:43:28 -0500

    Updated debian installer

--- install-debian.sh
+++ install-debian.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-URL='https://github.com/ChemicalDevelopment/ezc/releases/download/2.2.5/ezcc_amd64.deb'
+URL='https://github.com/ChemicalDevelopment/ezc/releases/download/2.2.5/ezcc_2.2.5_amd64.deb'
+echo "URL: $URL"
 FILE=`mktemp` 
 wget "$URL" -qO $FILE && sudo dpkg -i $FILE && echo "Installed"
+echo "Removing file"
 rm $FILE
 
