632bc6c8d09e294e365e204c4ebbadcbc6ad66cc
Cade Brown <brown.cade@gmail.com>
2016-12-30 16:29:14 -0500

    Build dir

--- scripts/req.sh
+++ scripts/req.sh
@@ -2,7 +2,7 @@
 GMP_TAR=https://gmplib.org/download/gmp/gmp-6.1.1.tar.xz
 MPFR_TAR=http://www.mpfr.org/mpfr-current/mpfr-3.1.5.tar.xz
 #$1
-BUILD_DIR=`readlink -f $1`
+BUILD_DIR=`python -c 'import os,sys;print(os.path.realpath("$1"))'`
 
 echo Installing GMP and MPFR in $BUILD_DIR
 
