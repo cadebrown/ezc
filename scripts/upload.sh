3658df4826d845db5429b923def3aa9179fd221f
Cade Brown <cade@cade.site>
2017-01-27 14:22:11 -0500

    Print out transfer

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -28,3 +28,5 @@ transfer() { if [ $# -eq 0 ]; then echo "No arguments specified. Usage:\necho tr
 tmpfile=$( mktemp -t transferXXX ); if tty -s; then basefile=$(basename "$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g'); curl --progress-bar --upload-file "$1" "https://transfer.sh/$basefile" >> $tmpfile; else curl --progress-bar --upload-file "-" "https://transfer.sh/$1" >> $tmpfile ; fi; cat $tmpfile; rm -f $tmpfile; }
 
 transfer $ARCHIVE
+
+printf "\n\nDone\n\n"
