3e84bebfc8083ad90e8d06e8a0689e1821c9e057
Cade Brown <brown.cade@gmail.com>
2017-03-19 21:06:19 -0400

    Updated indent

--- src/ezc.py
+++ src/ezc.py
@@ -1,4 +1,4 @@
-###             EZC src/ezc.py v@VERSION@
+###                     EZC src/ezc.py v@VERSION@
 #
 #  EZC is free software; you are free to modify and/or redistribute it under the terms of the GNU GPLv3. See 'LICENSE' for more details.
 #
