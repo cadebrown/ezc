da051bb81d48649c82487759ece3e22bdf98c3b0
Cade Brown <brown.cade@gmail.com>
2017-04-11 22:39:14 -0400

    EZC mini with mpfr and gmp support

--- mini.src/ezc_mini.c
+++ mini.src/ezc_mini.c
@@ -19,11 +19,88 @@ can also find a copy at http://www.gnu.org/licenses/.
 
 */
 
+
 #include <stdio.h>
 #include <stdlib.h> 
 #include <string.h>
 
 #include <math.h>
+
+#include "config.h"
+
+
+#define STRSIZE(bits) (long long)((bits) / (3.321))
+#define BITSIZE(len)  (long long)((len) * 3.321)
+
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
+
+
+#ifdef USE_GMP
+    #include <gmp.h>
+    #ifdef USE_MPFR
+        #include <mpfr.h>
+        #define FLOAT_TYPE mpfr_t
+        #define EZC_RND MPFR_RNDN
+
+        #define INIT(x) mpfr_init(x);
+        #define PREC(x) mpfr_get_prec(x)
+        #define SET(r, a) mpfr_set(r, a, EZC_RND);
+        #define SETPREC(r, a) mpfr_set_prec(r, MAX(a, min_prec));
+
+        #define SWAP(a, b) mpfr_swap(a, b);
+
+        #define ADD(r, a, b) mpfr_add(r, a, b, EZC_RND);
+        #define SUB(r, a, b) mpfr_sub(r, a, b, EZC_RND);
+        #define MUL(r, a, b) mpfr_mul(r, a, b, EZC_RND);
+        #define DIV(r, a, b) mpfr_div(r, a, b, EZC_RND);
+        #define POW(r, a, b) mpfr_pow(r, a, b, EZC_RND);
+
+        #define SQRT(r, a) mpfr_sqrt(r, a, EZC_RND);
+        #define LOG(r, a) mpfr_log(r, a, EZC_RND);
+        #define LOGB(r, a, b) mpfr_log(tmp_float, b, EZC_RND); mpfr_log(r, a, EZC_RND); DIV(r, a, tmp_float);
+
+        #define SIN(r, a) mpfr_sin(r, a, EZC_RND);
+        #define COS(r, a) mpfr_cos(r, a, EZC_RND);
+        #define TAN(r, a) mpfr_tan(r, a, EZC_RND);
+    #else
+        #define FLOAT_TYPE mpf_t
+
+        #define INIT(x) mpf_init(x);
+        #define PREC(x) mpf_get_prec(x)
+        #define SET(r, a) mpf_set(r, a);
+        #define SETPREC(r, a) mpf_set_prec(r, MAX(a, min_prec));
+
+        #define SWAP(a, b) mpf_swap(a, b);
+
+        #define ADD(r, a, b) mpf_add(r, a, b);
+        #define SUB(r, a, b) mpf_sub(r, a, b);
+        #define MUL(r, a, b) mpf_mul(r, a, b);
+        #define DIV(r, a, b) mpf_div(r, a, b);
+        #define POW(r, a, b) fail("POW not defined (use MPFR)");
+
+        #define SQRT(r, a) mpf_sqrt(r, a);
+        #define LOG(r, a) fail("LOG not defined (use MPFR)");
+        #define LOGB(r, a, b) fail("LOGB not defined (use MPFR)");
+
+        #define SIN(r, a) fail("SIN not defined (use MPFR)");
+        #define COS(r, a) fail("COS not defined (use MPFR)");
+        #define TAN(r, a) fail("TAN not defined (use MPFR)");
+    #endif
+
+#else
+    #define FLOAT_TYPE double
+    #define INIT(x) x = 0;
+    #define PREC(x) 53
+    
+    #define SET(r, a) r = a;
+
+    #define ADD(r, a, b) r = a + b
+    #define SUB(r, a, b) r = a - b
+    #define MUL(r, a, b) r = a * b
+    #define DIV(r, a, b) r = a / b
+    #define POW(r, a, b) r = pow(a, b)
+    #define SWAP(a, b) tmp_float = a; a = b; b = tmp_float;
+#endif
 
 #define KNRM  "\x1B[0m"
 #define KRED  "\x1B[31m"
@@ -43,43 +120,56 @@ can also find a copy at http://www.gnu.org/licenses/.
 
 #define STR_EQ(a, b) (strcmp(a, b) == 0)
 
-#define SUB2F(f) a = pop(); push(f(pop(), a))
-#define SUB1F(f) push(f(pop()))
-#define SUBO(o) a = pop(); push(pop() o a)
+#define STK(n) stack[n]
+#define RECENT(n) STK(stack_ptr - n)
+#define INC(n) inc(n)
 
-/* math functions */
-#define csc(x) (1.0 / sin(x))
-#define sec(x) (1.0 / cos(x))
-#define cot(x) (1.0 / tan(x))
-#define acsc(x) (asin(1.0 / x))
-#define asec(x) (acos(1.0 / x))
-#define acot(x) (atan(1.0 / x))
-#define logbase(x, y) (log(x) / log(y))
 
 int stack_ptr = -1;
-double stack[STACK_NUM];
+int max_stack_ptr = -1;
+FLOAT_TYPE stack[STACK_NUM];
+
+FLOAT_TYPE tmp_float;
+
+int min_prec = 53;
+
 char PSTR[PSTR_LEN];
 char *ccode;
 int cptr;
-double a, b;
 
+void inc(int n) {
+    if (n > 0) {
+        #ifdef USE_GMP
+            int i = stack_ptr + 1;
+            stack_ptr += n;
+            while (i <= stack_ptr) {
+                if (i > max_stack_ptr) {
+                    INIT(STK(i));
+                    max_stack_ptr = i;
+                }
+                i++;
+            }
+        #else
+            stack_ptr += n;
+        #endif
+    } else if (n < 0) {
+        stack_ptr += n;
+    }
 
-void push(double x) {
-    stack[++stack_ptr] = x;
-}
-
-double pop() {
-    return stack[stack_ptr--];
-}
-
-double get(int idx) {
-    return stack[idx];
 }
 
 void dump() {
     long long x = 0;
     while (x <= stack_ptr) {
-        printf(KCYN "%.17g" KNRM, stack[x]);
+        #ifdef USE_GMP
+            #ifdef USE_MPFR
+                mpfr_printf(KCYN "%.*Rf" KNRM, STRSIZE(PREC(STK(x))), STK(x));
+            #else
+                gmp_printf(KCYN "%.*Ff" KNRM, STRSIZE(PREC(STK(x))), STK(x));
+            #endif
+        #else
+            printf(KCYN "%.17g" KNRM, STK(x));
+        #endif
         if (x != stack_ptr) {
             printf(KBLU ", " KNRM);
         }
@@ -129,39 +219,73 @@ void eval(char *code) {
                 PSTR[j++] = code[i++];
             }
             PSTR[j] = 0;
-            push(strtod(PSTR, NULL));
+            INC(1);
+            #ifdef USE_GMP
+                SETPREC(RECENT(0), BITSIZE(strlen(PSTR)))
+                #ifdef USE_MPFR
+                    mpfr_set_str(RECENT(0), PSTR, 10, EZC_RND);
+                #else
+                    mpf_set_str(RECENT(0), PSTR, 10);
+                #endif
+            #else
+                RECENT(0) = strtod(PSTR, NULL);
+            #endif
         } else if (code[i] == '+') {
-            SUBO(+);
+            ADD(RECENT(1), RECENT(1), RECENT(0))
+            INC(-1);
             i++;
         } else if (code[i] == '-') {
-            SUBO(-);
+            SUB(RECENT(1), RECENT(1), RECENT(0))
+            INC(-1);
             i++;
         } else if (code[i] == '*') {
-            SUBO(*);
+            MUL(RECENT(1), RECENT(1), RECENT(0))
+            INC(-1);
             i++;
         } else if (code[i] == '/') {
-            SUBO(/);
+            DIV(RECENT(1), RECENT(1), RECENT(0))
+            INC(-1);
             i++;
         } else if (code[i] == '^') {
-            SUB2F(pow);
+            POW(RECENT(1), RECENT(1), RECENT(0))
+            INC(-1);
             i++;
         } else if (code[i] == '>') {
-            stack_ptr++;
+            INC(1);
             i++;
         } else if (code[i] == '<') {
-            stack_ptr--;
+            INC(-1);
             i++;
         } else if (code[i] == ':') {
-            a = pop();
-            b = pop();
-            push(a);
-            push(b);
+            SWAP(RECENT(0), RECENT(1));
             i++;
         } else if (code[i] == '.') {
-            push(get(stack_ptr));
+            INC(1);
+            SET(RECENT(0), RECENT(1));
             i++;
         } else if (code[i] == '$') {
-            SUB1F(get);
+            #ifdef USE_GMP
+                #ifdef USE_MPFR
+                    SET(RECENT(0), STK(mpfr_get_ui(RECENT(0), EZC_RND)));
+                #else
+                    SET(RECENT(0), STK(mpf_get_ui(RECENT(0))));
+                #endif
+            #else
+                RECENT(0) = STK(RECENT(0));
+            #endif
+            i++;
+        } else if (code[i] == '_') {
+            #ifdef USE_GMP
+                INC(1);
+                #ifdef USE_MPFR
+                    mpfr_set_ui(RECENT(0), stack_ptr+1, EZC_RND);
+                #else
+                    mpf_set_ui(RECENT(0), stack_ptr+1);
+                #endif
+            #else
+                INC(1)
+                RECENT(0) = stack_ptr+1;
+            #endif
             i++;
         } else if (IS_ALPHA(code[i])) {
             j = 0;
@@ -169,27 +293,12 @@ void eval(char *code) {
                 PSTR[j++] = code[i++];
             }
             PSTR[j] = 0;
-            /*  */ if (STR_EQ(PSTR, "sin")) { SUB1F(sin);
-            } else if (STR_EQ(PSTR, "cos")) { SUB1F(cos);
-            } else if (STR_EQ(PSTR, "tan")) { SUB1F(tan);
-            } else if (STR_EQ(PSTR, "csc")) { SUB1F(csc);
-            } else if (STR_EQ(PSTR, "sec")) { SUB1F(sec);
-            } else if (STR_EQ(PSTR, "cot")) { SUB1F(cot);
-            } else if (STR_EQ(PSTR, "asin")) { SUB1F(asin);
-            } else if (STR_EQ(PSTR, "acos")) { SUB1F(acos);
-            } else if (STR_EQ(PSTR, "atan")) { SUB1F(atan);
-            } else if (STR_EQ(PSTR, "acsc")) { SUB1F(acsc);
-            } else if (STR_EQ(PSTR, "asec")) { SUB1F(asec);
-            } else if (STR_EQ(PSTR, "acot")) { SUB1F(acot);
-            /* hyperbolic                                */
-            } else if (STR_EQ(PSTR, "sinh")) { SUB1F(sinh);
-            } else if (STR_EQ(PSTR, "cosh")) { SUB1F(cosh);
-            } else if (STR_EQ(PSTR, "tanh")) { SUB1F(tanh);
-            /* most of math.h                            */
-            } else if (STR_EQ(PSTR, "sqrt")) { SUB1F(sqrt);
-            } else if (STR_EQ(PSTR, "log")) { SUB1F(log);
-            } else if (STR_EQ(PSTR, "logb")) { SUB2F(logbase);
-            /* err                                       */
+             if (STR_EQ(PSTR, "sin"))        { SIN(RECENT(0), RECENT(0));
+            } else if (STR_EQ(PSTR, "cos"))  { COS(RECENT(0), RECENT(0));
+            } else if (STR_EQ(PSTR, "tan"))  { TAN(RECENT(0), RECENT(0));
+            } else if (STR_EQ(PSTR, "sqrt")) { SQRT(RECENT(0), RECENT(0));
+            } else if (STR_EQ(PSTR, "log"))  { LOG(RECENT(0), RECENT(0));
+            } else if (STR_EQ(PSTR, "logb")) { LOGB(RECENT(1), RECENT(1), RECENT(0)); INC(-1);
             } else {
                 char estr[PSTR_LEN];
                 sprintf(estr, "Unknown function %s\n", PSTR);
@@ -204,6 +313,13 @@ void eval(char *code) {
 
 
 int main(int argc, char *argv[]) {
+    #ifdef USE_GMP
+        mpf_set_default_prec(100);
+    #endif
+
+    INIT(tmp_float);
+
+
     eval(argv[1]);
     dump();
 }
